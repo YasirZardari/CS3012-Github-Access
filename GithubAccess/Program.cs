89c7d32a9041fdc7f6fbf385a8e8db7a0faab277
YasirZardari <yasirzardari@gmail.com>
2017-11-19 00:43:07 +0000

    Add sample Github API program

--- GithubAccess/Program.cs
+++ GithubAccess/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Octokit;
 
 namespace GithubAccess
 {
@@ -10,6 +11,20 @@ namespace GithubAccess
     {
         static void Main(string[] args)
         {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("this-is-Yasir"));
+            var user = await client.User.Get("YasirZardari");
+            Console.WriteLine("{0} has {1} public repositories - go check out their profile at {2}",
+                user.Name,
+                user.PublicRepos,
+                user.Url);
+            Console.WriteLine(user.CreatedAt);
+            Console.ReadLine();
         }
     }
 }
+
