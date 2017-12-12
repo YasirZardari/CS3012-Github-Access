d351346d64211a3e1a985b4e2fe8efbbb4404680
YasirZardari <yasirzardari@gmail.com>
2017-12-12 20:54:19 +0000

    Added counts of repo by language for user

--- GithubAccess/Program.cs
+++ GithubAccess/Program.cs
@@ -22,8 +22,8 @@ namespace GithubAccess
             string username = Console.ReadLine();
             Console.Write("Please enter password: ");
             string password = Console.ReadLine();
-          //  Console.Write("Please enter user you want to retrieve data for: ");
-            //String GithubUser = Console.ReadLine();
+            Console.Write("Please enter user you want to retrieve data for: ");
+            String GithubUser = Console.ReadLine();
             var basicAuth = new Credentials(username, password); // NOTE: not real credentials
             client.Credentials = basicAuth;
 
@@ -45,18 +45,19 @@ namespace GithubAccess
                        Console.ReadLine();
                        //Test comment
                    */
-            var repos = await client.Repository.GetAllForCurrent();
+            var repos = await client.Repository.GetAllForUser(GithubUser);
             var count = repos.Count;
 
             var JavaCount = 0;
             var C_Count = 0;
             var PythonCount = 0;
             var JavascriptCount = 0;
-            var HtmlCount = 0;
+            var PHPCount = 0;
             var CSharpCount = 0;
             var RubyCount = 0;
             var ObjC_Count = 0;
             var CplusCount = 0;
+            var OtherCount = 0;
             //var lang = repos.ElementAt(0).Language;
             Console.WriteLine("number of repos: " + count);
             for (int i = 0; i < count; i++)
@@ -69,12 +70,57 @@ namespace GithubAccess
              {
                     JavaCount++;
              }
-             
-           
-            }
-            Console.WriteLine(JavaCount);
+             else if (String.Compare("C", repoLanguage) == 0)
+             {
+                    C_Count++;
+             }
+             else if (String.Compare("C#", repoLanguage) == 0)
+             {
+                    CSharpCount++;
+             }
+             else if (String.Compare("PHP", repoLanguage) == 0)
+             {
+                    PHPCount++;
+             }
+             else if (String.Compare("Objective-C", repoLanguage) == 0)
+             {
+                    ObjC_Count++;
+             }
+             else if (String.Compare("JavaScript", repoLanguage) == 0)
+             {
+                    JavascriptCount++;
+             }
+             else if (String.Compare("Python", repoLanguage) == 0)
+             {
+                    PythonCount++;
+             }
+             else if (String.Compare("C++", repoLanguage) == 0)
+             {
+                    CplusCount++;
+             }
+             else if (String.Compare("Ruby", repoLanguage) == 0)
+             {
+                    RubyCount++;
+             }
+                else
+             {
+                    OtherCount++;
+             }
 
-           
+
+            }
+            Console.WriteLine("Java - " + JavaCount);
+            Console.WriteLine("Python - " + PythonCount);
+            Console.WriteLine("C# - " + CSharpCount);
+            Console.WriteLine("C++ - " + CplusCount);
+            Console.WriteLine("ObjC - " + ObjC_Count);
+            Console.WriteLine("JavaScript - " + JavascriptCount);
+            Console.WriteLine("Ruby - " + RubyCount);
+            Console.WriteLine("PHP - " + PHPCount);
+            Console.WriteLine("C - " + C_Count);
+            Console.WriteLine("Other - " + OtherCount);
+
+
             Console.ReadLine();
         }
     }
